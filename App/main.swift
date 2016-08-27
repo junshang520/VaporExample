b73b0b53ba7708586af70fbba71a02762b727740
Sahand Edrisian <sahandedrisian@Sahands-MacBook-Pro.local>
2016-08-26 17:54:30 -0700

    init

--- App/main.swift
+++ App/main.swift
@@ -7,7 +7,10 @@ drop.get("/") { request in
 }
 
 drop.get("/name",":name") { request in
-    return "Hello \(request.parameters["name"].string!)!"
+    if let name = request.parameters["name"].string {
+        return "Hello \(name)!"
+    }
+    return "Error retrieving parameters."
 }
 
 drop.get("/view") { request in
